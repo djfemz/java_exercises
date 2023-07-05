5e0754bd40555c6209ba3a8e184bf4513a926fbb
djfemz <oladejifemi00@gmail.com>
2023-07-05 11:04:15 +0100

    added CalculatorTest

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
     @Test
     public void addTest(){
         assertEquals(5, 5);
+        assertTrue(true);
+        assertFalse(false);
     }
 }
